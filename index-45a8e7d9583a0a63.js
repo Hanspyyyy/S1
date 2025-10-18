991fefd7b73071fc0089d8af9248c564aae6e8d9
Hanspyyyy <hakimanhnns@gmail.com>
2025-10-18 20:13:19 +0700

    Create index-45a8e7d9583a0a63.js

---
+++ index-45a8e7d9583a0a63.js
@@ -0,0 +1,277 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([[405], {
+    5728: function(e, a, t) {
+        (window.__NEXT_P = window.__NEXT_P || []).push(["/", function() {
+            return t(5400)
+        }
+        ])
+    },
+    181: function(e, a, t) {
+        "use strict";
+        var i = t(5893)
+          , n = t(5675)
+          , l = t.n(n);
+        a.Z = () => (0,
+        i.jsx)("div", {
+            className: "hidden xl:flex xl:max-w-none pointer-events-none select-none",
+            children: (0,
+            i.jsx)(l(), {
+                src: "/avatar.png",
+                alt: "avatar",
+                width: 737,
+                height: 678,
+                className: "translate-z-0 w-full h-full"
+            })
+        })
+    },
+    5400: function(e, a, t) {
+        "use strict";
+        t.r(a),
+        t.d(a, {
+            default: function() {
+                return b
+            }
+        });
+        var i = t(5893)
+          , n = t(2951)
+          , l = t(7294)
+          , s = t(5481)
+          , r = t(7918)
+          , o = () => {
+            let e = (0,
+            l.useCallback)(async e => {
+                await (0,
+                r.R)(e)
+            }
+            , [])
+              , a = (0,
+            l.useCallback)(async () => {}
+            , []);
+            return (0,
+            i.jsx)(s.D, {
+                className: "w-full h-full absolute translate-z-0",
+                id: "tsparticles",
+                init: e,
+                loaded: a,
+                options: {
+                    fullScreen: {
+                        enable: !1
+                    },
+                    background: {
+                        color: {
+                            value: ""
+                        }
+                    },
+                    fpsLimit: 120,
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: !1,
+                                mode: "push"
+                            },
+                            onHover: {
+                                enable: !0,
+                                mode: "repulse"
+                            },
+                            resize: !0
+                        },
+                        modes: {
+                            push: {
+                                quantity: 90
+                            },
+                            repulse: {
+                                distance: 200,
+                                duration: .4
+                            }
+                        }
+                    },
+                    particles: {
+                        color: {
+                            value: "#e68e2e"
+                        },
+                        links: {
+                            color: "#f5d393",
+                            distance: 150,
+                            enable: !0,
+                            opacity: .5,
+                            width: 1
+                        },
+                        collisions: {
+                            enable: !0
+                        },
+                        move: {
+                            direction: "none",
+                            enable: !0,
+                            outMode: {
+                                default: "bounce"
+                            },
+                            random: !1,
+                            speed: 1,
+                            straight: !1
+                        },
+                        number: {
+                            density: {
+                                enable: !0,
+                                area: 800
+                            },
+                            value: 80
+                        },
+                        opacity: {
+                            value: .5
+                        },
+                        shape: {
+                            type: "circle"
+                        },
+                        size: {
+                            value: {
+                                min: 1,
+                                max: 5
+                            }
+                        }
+                    },
+                    detectRetina: !0
+                }
+            })
+        }
+          , d = t(5675)
+          , c = t.n(d)
+          , u = t(1664)
+          , x = t.n(u)
+          , m = t(9274)
+          , h = () => (0,
+        i.jsx)("div", {
+            className: "mx-auto xl:mx-0",
+            children: (0,
+            i.jsxs)(x(), {
+                href: "/work",
+                className: "relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group",
+                children: [(0,
+                i.jsx)(c(), {
+                    src: "/rounded-text.png",
+                    alt: "rounded text",
+                    width: 141,
+                    height: 148,
+                    className: "animate-spin-slow w-full h-full max-w-[141px] max-h-[148px] pointer-events-none select-none"
+                }), (0,
+                i.jsx)(m.WY3, {
+                    className: "absolute text-4xl group-hover:translate-x-2 transition-all duration-300",
+                    "aria-hidden": !0
+                })]
+            })
+        })
+          , p = t(181)
+          , f = t(2204)
+          , b = () => (0,
+        i.jsxs)("div", {
+            className: "bg-primary/60 h-full",
+            children: [(0,
+            i.jsx)("div", {
+                className: "w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10",
+                children: (0,
+                i.jsxs)("div", {
+                    className: "text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto",
+                    children: [(0,
+                    i.jsxs)(n.E.h1, {
+                        variants: (0,
+                        f.J)("down", .2),
+                        initial: "hidden",
+                        animate: "show",
+                        exit: "hidden",
+                        className: "h1",
+                        children: ["Transforming Ideas ", (0,
+                        i.jsx)("br", {}), " Into", " ", (0,
+                        i.jsx)("span", {
+                            className: "text-accent",
+                            children: "Digital Reality"
+                        })]
+                    }), (0,
+                    i.jsx)(n.E.p, {
+                        variants: (0,
+                        f.J)("down", .3),
+                        initial: "hidden",
+                        animate: "show",
+                        exit: "hidden",
+                        className: "max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16",
+                        children: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate, exercitationem harum, quia nulla temporibus deleniti libero veniam vero beatae numquam ducimus illum ab similique ipsam tempore fugit quod laudantium debitis."
+                    }), (0,
+                    i.jsx)("div", {
+                        className: "flex justify-center xl:hidden relative",
+                        children: (0,
+                        i.jsx)(h, {})
+                    }), (0,
+                    i.jsx)(n.E.div, {
+                        variants: (0,
+                        f.J)("down", .4),
+                        initial: "hidden",
+                        animate: "show",
+                        exit: "hidden",
+                        className: "hidden xl:flex",
+                        children: (0,
+                        i.jsx)(h, {})
+                    })]
+                })
+            }), (0,
+            i.jsxs)("div", {
+                className: "w-[1280px] h-full absolute right-0 bottom-0",
+                children: [(0,
+                i.jsx)("div", {
+                    role: "img",
+                    className: "bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0",
+                    "aria-hidden": !0
+                }), (0,
+                i.jsx)(o, {}), (0,
+                i.jsx)(n.E.div, {
+                    variants: (0,
+                    f.J)("up", .5),
+                    initial: "hidden",
+                    animate: "show",
+                    exit: "hidden",
+                    transition: {
+                        duration: 1,
+                        ease: "easeInOut"
+                    },
+                    className: "w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]",
+                    children: (0,
+                    i.jsx)(p.Z, {})
+                })]
+            })]
+        })
+    },
+    2204: function(e, a, t) {
+        "use strict";
+        t.d(a, {
+            J: function() {
+                return i
+            }
+        });
+        let i = (e, a) => ({
+            hidden: {
+                y: "up" === e ? 80 : "down" === e ? -80 : 0,
+                opacity: 0,
+                x: "left" === e ? 80 : "right" === e ? -80 : 0,
+                transition: {
+                    type: "tween",
+                    duration: 1.5,
+                    delay: a,
+                    ease: [.25, .6, .3, .8]
+                }
+            },
+            show: {
+                y: 0,
+                x: 0,
+                opacity: 1,
+                transition: {
+                    type: "tween",
+                    duration: 1.4,
+                    delay: a,
+                    ease: [.25, .25, .25, .75]
+                }
+            }
+        })
+    }
+}, function(e) {
+    e.O(0, [412, 888, 774, 179], function() {
+        return e(e.s = 5728)
+    }),
+    _N_E = e.O()
+}
+]);
